b0c3d2684791d42053753265b045590dc7d4be00
Aditya Shukla <aditya.shukla@gorapid.io>
2020-09-10 15:09:04 +0530

    Added read_all() API handler
      * ValueError: Invalid fields for <NotePersonSchema(many=False)>: {'notes'}
      * Getting error while calling the API in swager UI

--- notes.py
+++ notes.py
@@ -1,4 +1,22 @@
+from flask import make_response, abort
+from config import db
 from models import Person, Note, NoteSchema
+
+def read_all():
+    """
+    This function responds to a request for /api/people/notes
+    with the complete list of notes, sorted by note timestamp
+    :return:                json list of all notes for all people
+    """
+    # Query the database for all the notes
+    notes = Note.query.order_by(db.desc(Note.timestamp)).all()
+
+    # Serialize the list of notes from our data
+    note_schema = NoteSchema(many=True, exclude=["person.notes"])
+    data = note_schema.dump(notes).data
+    return data
+    pass
+
 def read_one(person_id, note_id):
     """
     This function responds to a request for
@@ -26,3 +44,12 @@ def read_one(person_id, note_id):
     # Otherwise, nope, didn't find that note
     else:
         abort(404, f"Note not found for Id: {note_id}")
+
+def update(person_id, noteid):
+    pass
+
+def create(person_id, note_id):
+    pass
+
+def delete(person_id, note_id):
+    pass
