4900dfaec8dba4f85343b833f393b988ad717270
Aditya Shukla <aditya.shukla@gorapid.io>
2020-09-10 17:36:51 +0530

    Addedd other API Handlers in note module
      * POST, PUT, DELETE handlers added for performing add/update/delete  operations

--- notes.py
+++ notes.py
@@ -2,6 +2,7 @@ from flask import make_response, abort
 from config import db
 from models import Person, Note, NoteSchema
 
+
 def read_all():
     """
     This function responds to a request for /api/people/notes
@@ -16,6 +17,7 @@ def read_all():
     data = note_schema.dump(notes)
     return data
     pass
+
 
 def read_one(person_id, note_id):
     """
@@ -45,11 +47,97 @@ def read_one(person_id, note_id):
     else:
         abort(404, f"Note not found for Id: {note_id}")
 
-def update(person_id, noteid):
-    pass
 
-def create(person_id, note_id):
-    pass
+def create(person_id, note):
+    """
+    This function creates a new note related to the passed in person id.
+    :param person_id:       Id of the person the note is related to
+    :param note:            The JSON containing the note data
+    :return:                201 on success
+    """
+    # get the parent person
+    person = Person.query.filter(Person.person_id == person_id).one_or_none()
+
+    # Was a person found?
+    if person is None:
+        abort(404, f"Person not found for Id: {person_id}")
+
+    # Create a note schema instance
+    schema = NoteSchema()
+    new_note = schema.load(note, session=db.session)
+
+    # Add the note to the person and database
+    person.notes.append(new_note)
+    db.session.commit()
+
+    # Serialize and return the newly created note in the response
+    data = schema.dump(new_note)
+
+    return data, 201
+
+
+def update(person_id, note_id, note):
+    """
+    This function updates an existing note related to the passed in
+    person id.
+    :param person_id:       Id of the person the note is related to
+    :param note_id:         Id of the note to update
+    :param content:            The JSON containing the note data
+    :return:                200 on success
+    """
+    update_note = (
+        Note.query.filter(Person.person_id == person_id)
+        .filter(Note.note_id == note_id)
+        .one_or_none()
+    )
+
+    # Did we find an existing note?
+    if update_note is not None:
+
+        # turn the passed in note into a db object
+        schema = NoteSchema()
+        update = schema.load(note, session=db.session)
+
+        # Set the id's to the note we want to update
+        update.person_id = update_note.person_id
+        update.note_id = update_note.note_id
+
+        # merge the new object into the old and commit it to the db
+        db.session.merge(update)
+        db.session.commit()
+
+        # return updated note in the response
+        data = schema.dump(update_note)
+
+        return data, 200
+
+    # Otherwise, nope, didn't find that note
+    else:
+        abort(404, f"Note not found for Id: {note_id}")
+
 
 def delete(person_id, note_id):
-    pass
+    """
+    This function deletes a note from the note structure
+    :param person_id:   Id of the person the note is related to
+    :param note_id:     Id of the note to delete
+    :return:            200 on successful delete, 404 if not found
+    """
+    # Get the note requested
+    note = (
+        Note.query.filter(Person.person_id == person_id)
+        .filter(Note.note_id == note_id)
+        .one_or_none()
+    )
+
+    # did we find a note?
+    if note is not None:
+        db.session.delete(note)
+        db.session.commit()
+        return make_response(
+            "Note {note_id} deleted".format(note_id=note_id), 200
+        )
+
+    # Otherwise, nope, didn't find that note
+    else:
+        abort(404, f"Note not found for Id: {note_id}")
